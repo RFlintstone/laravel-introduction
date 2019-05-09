4f4461ec94c9641e9d099418b7bd13d34d550f4d
RFlintstone <rubenflint@hotmail.com>
2019-05-09 09:11:06 +0200

    First commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<h1>Contact</h1>
+<form>
+    <label for="email">Email:</label>
+    <input type="email" id="email">
+    <br>
+    <label for="msg">Message:</label>
+    <input type="text" id="msg">
+</form>
+<?php
